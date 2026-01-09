c8d5c5d943777124c0a70ced2d214a8bc357412e
TP9NS <pshccxx@gmail.com>
2026-01-09 23:19:51 +0900

    project 초기 설정

---
+++ src/main/java/com/flow/fileExtension/FileExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.fileExtension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExtensionApplication.class, args);
+	}
+
+}
